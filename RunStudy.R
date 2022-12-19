0c80478ac76eaa4e3817d688ccd41b5227066964
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2022-12-19 20:51:27 +0000

    Adding in files

---
+++ RunStudy.R
@@ -0,0 +1,35 @@
+
+# table names----
+outcome_table_name<-paste0(outcome_table_stem,"_o")
+strata_table_name<-paste0(outcome_table_stem,"_strata")
+
+# output files ----
+if (!file.exists(output.folder)){
+  dir.create(output.folder, recursive = TRUE)}
+
+start<-Sys.time()
+
+# start log ----
+log_file <- paste0(output.folder, "/log.txt")
+logger <- create.logger()
+logfile(logger) <- log_file
+level(logger) <- "INFO"
+
+# instantiate study cohorts ----
+info(logger, 'INSTANTIATING STUDY COHORTS')
+source(here("1_InstantiateCohorts","InstantiateStudyCohorts.R"))
+info(logger, 'GOT STUDY COHORTS')
+
+# Run incidence rate analysis ----
+info(logger, 'RUNNING INCIDENCE RATE ANALYSIS')
+source(here("2_Analysis","IncidenceAnalysis.R"))
+info(logger, 'INCIDENCE RATE ANALYSIS RAN')
+
+# add code for combining and exporting results
+
+print("Done!")
+print("-- If all has worked, there should now be a zip folder with your results in the output folder to share")
+print("-- Thank you for running the study!")
+Sys.time()-start
+readLines(log_file)
+
