9a61d252f62f6466ce4f32eaf2512baed36e1eba
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-01-19 16:06:51 +0000

    update

--- CodeToRun.R
+++ CodeToRun.R
@@ -19,8 +19,8 @@ library(ggplot2)
 
 # database metadata and connection details -----
 # The name/ acronym for the database
-#db.name<-"CPRDAurum"
-db.name<-"CPRDGold"
+db.name<-"CPRDAurum"
+#db.name<-"CPRDGold"
 
 # Set output folder location -----
 # the path to a folder where the results from this analysis will be saved
@@ -35,8 +35,8 @@ user<-Sys.getenv("DB_USER")
 password<- Sys.getenv("DB_PASSWORD")
 port<-Sys.getenv("DB_PORT") 
 host<-Sys.getenv("DB_HOST") 
-#server_dbi<-Sys.getenv("DB_SERVER_cdm_aurum_202106_dbi") #aurum
-server_dbi<-Sys.getenv("DB_SERVER_cdmgold202007_dbi") #gold
+server_dbi<-Sys.getenv("DB_SERVER_cdm_aurum_202106_dbi") #aurum
+#server_dbi<-Sys.getenv("DB_SERVER_cdmgold202007_dbi") #gold
 
 
 # Specify cdm_reference via DBI connection details -----
