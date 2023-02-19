a8ec1418c201201038adf5fb5d2f00020fb2db67
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-02-19 17:15:35 +0000

    update

--- CodeToRun.R
+++ CodeToRun.R
@@ -1,5 +1,5 @@
 # Load packages ------
-#renv::activate()
+renv::activate()
 renv::restore()
 # to install latest version of IncidencePrevalence
 #remotes::install_github("darwin-eu-dev/IncidencePrevalence",force = TRUE)
@@ -17,8 +17,6 @@ library(tidyr)
 library(stringr)
 library(CDMConnector)
 library(ggplot2)
-library(RPostgres)
-library(lubridate)
 library(broom)
 library(survival)
 library(bshazard)
@@ -26,8 +24,8 @@ library(bshazard)
 # database metadata and connection details -----
 # The name/ acronym for the database
 #db.name<-"CPRDAurum"
-db.name<-"CPRDGold"
-#db.name<-"CPRDAurumCovid"
+#db.name<-"CPRDGold"
+db.name<-"CPRDAurumCovid"
 
 # Set output folder location -----
 # the path to a folder where the results from this analysis will be saved
@@ -43,8 +41,8 @@ password<- Sys.getenv("DB_PASSWORD")
 port<-Sys.getenv("DB_PORT") 
 host<-Sys.getenv("DB_HOST") 
 #server_dbi<-Sys.getenv("DB_SERVER_cdm_aurum_202106_dbi") #aurum
-server_dbi<-Sys.getenv("DB_SERVER_cdmgold202007_dbi") #gold
-#server_dbi<-Sys.getenv("DB_SERVER_p20_059_cdm_aurum_dbi") #aurum covid
+#server_dbi<-Sys.getenv("DB_SERVER_cdmgold202007_dbi") #gold
+server_dbi<-Sys.getenv("DB_SERVER_p20_059_cdm_aurum_dbi") #aurum covid
 
 
 # Specify cdm_reference via DBI connection details -----
