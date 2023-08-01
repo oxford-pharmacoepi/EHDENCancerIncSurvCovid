bae866951327d4661d56f7e22975329cd51bcf13
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2023-08-01 12:27:23 +0100

    update

---
+++ 5_tables/tables4papers.R
@@ -0,0 +1,172 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(here)
+library(quarto)
+library(ggplot2)
+library(scales)
+library(ggh4x)
+library(readr)
+library(rio)
+library(tidyverse)
+
+datapath <- "C:/Users/dnewby/Documents/GitHub/EHDENCancerIncidencePrevalence/5_tables/data"
+
+#printing numbers with 3 decimal place and commas
+nice.num3<-function(x) {
+  trimws(format(round(x,3),
+                big.mark=",", nsmall = 3, digits=3, scientific=FALSE))}
+
+nice.num2<-function(x) {
+  trimws(format(round(x,2),
+                big.mark=",", nsmall = 2, digits=2, scientific=FALSE))}
+
+nice.num1<-function(x) {
+  trimws(format(round(x,1),
+                big.mark=",", nsmall = 1, digits=1, scientific=FALSE))}
+
+
+# for survival probabilites
+prepare_output_survival <- function(result){
+  result <- result %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerHypopharynx", "Hypopharynx")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerLarynx", "Larynx")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerNasalCavitySinus", "Nasal Cavity & Sinus")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerNasopharynx", "Nasopharynx")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerOralCavityPrevalent", "Oral Cavity")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerOropharynx", "Oropharynx")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerSalivaryGland", "Salivary Gland")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerTonguePrevalent", "Tongue")) %>% 
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerOralCavityIncidence", "Oral Cavity")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "HeadNeckSubtypeCancerTongueIncidence", "Tongue")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentProstateCancer", "Prostate")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentLungCancer", "Lung")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentBreastCancer", "Breast")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentColorectalCancer", "Colorectal")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentHeadNeckCancer", "Head & Neck")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentLiverCancer", "Liver")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentPancreaticCancer", "Pancreas")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentStomachCancer", "Stomach")) %>%
+    mutate(Cancer = replace(Cancer, Cancer == "IncidentEsophagealCancer", "Oesophagus")) %>%    
+    mutate(Cancer = replace(Cancer, Cancer == "PrevalentProstateCancer", "Prostate")) 
+  
+  
+  result<- result %>% 
+    mutate(Age = replace(Age, Age == ">=90", "90 +")) %>% 
+    mutate(Age = replace(Age, Age == "<30", "18-29")) %>% 
+    mutate(Age= stringr::str_replace(Age, "-", " to ")) %>% 
+    mutate(CalenderYearGp= stringr::str_replace(CalenderYearGp, "-", " to ")) %>% 
+    mutate(Age = factor(Age,
+                        levels = c("All",
+                                   "18 to 29", "30 to 39", "40 to 49",
+                                   "50 to 59", "60 to 69", "70 to 79",
+                                   "80 to 89", "90 +" ))) %>%
+    mutate(CalenderYearGp = factor(CalenderYearGp,
+                                   levels = c("2000 to 2019",
+                                              "2000 to 2021",
+                                              "2000 to 2004", 
+                                              "2005 to 2009", 
+                                              "2010 to 2014",
+                                              "2015 to 2019",
+                                              "2020 to 2021")))
+  
+  
+  result <- result %>%
+    mutate(Database = replace(Database, Database == "CPRDAurum", "CPRD Aurum")) %>%
+    mutate(Database = replace(Database, Database == "CPRDGoldUpdate", "CPRD GOLD")) 
+  
+  result <- result %>%
+    mutate(Gender=replace(Gender, Cancer=="Prostate", "Male"))
+  
+  return(result)
+} 
+
+# Load, prepare, and merge results -----
+results <-list.files(datapath, full.names = TRUE,
+                     recursive = TRUE,
+                     include.dirs = TRUE,
+                     pattern = ".zip")
+
+#unzip data
+for (i in (1:length(results))) {
+  utils::unzip(zipfile = results[[i]],
+               exdir = datapath)
+}
+
+#grab the results from the folders
+results <- list.files(
+  path = datapath,
+  pattern = ".csv",
+  full.names = TRUE,
+  recursive = TRUE,
+  include.dirs = TRUE
+)
+
+
+# merge the survival probabilities estimates
+survival_probabilites_files<-results[stringr::str_detect(results, ".csv")]
+survival_probabilites_files<-results[stringr::str_detect(results, "one_five_ten_survival_rates")]
+
+
+survival_probabilites <- list()
+for(i in seq_along(survival_probabilites_files)){
+  survival_probabilites[[i]]<-readr::read_csv(survival_probabilites_files[[i]],
+                                             show_col_types = FALSE)
+}
+
+survival_probabilites <- dplyr::bind_rows(survival_probabilites) 
+survival_probabilites <- prepare_output_survival(survival_probabilites) %>%
+  mutate("Survival Rate % (95% CI)"= ifelse(!is.na(surv),
+                                       paste0(paste0(nice.num1(surv*100)), " (",
+                                              paste0(nice.num1(lower*100))," - ",
+                                              paste0(nice.num1(upper*100)), ")"),
+                                       NA
+  ))
+
+saveRDS(survival_probabilites,
+        paste0(datapath, "/survival_probabilites.rds"))
+
+
+# table for short term survival for covid paper
+# filter to include 1 and 2 year survival
+
+survival_probabilites12 <- survival_probabilites %>% 
+  filter(time == 1 | time == 2) %>% 
+  select(time, Gender, Cancer, CalenderYearGp, `Survival Rate % (95% CI)`)
+
+# convert from long to wide format based on time column
+survival_probabilites_table <- pivot_wider(survival_probabilites12, names_from = "time", values_from = c(`Survival Rate % (95% CI)`)) %>% 
+  rename(`% One year survival (95% CI)` = "1", `% Two year survival (95% CI)` = "2" ) 
+
+# extract data for prostate cancer males
+survival_probabilites_table_prostate <-
+  survival_probabilites_table %>% 
+  filter(Cancer == "Prostate")
+
+#extract results for female breast cancer patients
+survival_probabilites_table_breast <-
+  survival_probabilites_table %>% 
+  filter(Cancer == "Breast" & Gender == "Female")
+
+# rwo bind relevant results together
+survival_probabilites_final <- 
+  survival_probabilites_table %>% 
+  filter(Gender == "Both") %>% 
+  filter(Cancer != "Breast")
+
+# create final table remove and rename columns
+survival_probabilites_final <- rbind(survival_probabilites_final,
+                                     survival_probabilites_table_breast,
+                                     survival_probabilites_table_prostate ) %>% 
+  relocate(`Calendar Year` =CalenderYearGp, .after = last_col()) %>% 
+  select(-c(Gender)) %>% 
+  arrange(Cancer)
+
+# save as csv
+write.csv(survival_probabilites_final ,file = paste0(datapath, "/survival_probabilites_covid_paper.csv"))
+
+
+
+
+
+
